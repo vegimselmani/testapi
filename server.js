446f203df68f0b497f8c4f0dbc2cbbca3a8c1470
vegimselmani <vegim.selmani@drweb.mk>
2022-04-27 23:59:37 +0200

    cors test2

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var express  = require('express')
 var app  = express()
-import cors from 'cors';
-app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
- });
+
+const server1 = jsonServer.create();
+const middlewares = jsonServer.defaults({ noCors: true })
+
+// set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
 var server = app.listen(process.env.PORT || 5000, () => {
     console.log('Server is started on 127.0.0.1:'+ (process.env.PORT || 5000))
 });
