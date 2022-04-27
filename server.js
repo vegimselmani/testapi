496ef433eaf4ba131af1fd8c0fe58e18c45cbc86
vegimselmani <vegim.selmani@drweb.mk>
2022-04-27 23:36:31 +0200

    changed port to 5000

---
+++ server.js
@@ -0,0 +1,5 @@
+var express  = require('express')
+var app  = express()
+var server = app.listen(process.env.PORT || 5000, () => {
+    console.log('Server is started on 127.0.0.1:'+ (process.env.PORT || 5000))
+})
