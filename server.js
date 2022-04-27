fccf4691cb64302530481dd405b179507db434cc
vegimselmani <vegim.selmani@drweb.mk>
2022-04-27 23:49:18 +0200

    cors v1

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express  = require('express')
 var app  = express()
-var server = app.listen(process.env.PORT || 5000, () => {
-    console.log('Server is started on 127.0.0.1:'+ (process.env.PORT || 5000))
+import cors from 'cors';
+app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authortization');
+    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 })
 
-let headers = new Headers();
-  headers.append('Access-Control-Allow-Origin');
+var server = app.listen(process.env.PORT || 5000, () => {
+    console.log('Server is started on 127.0.0.1:'+ (process.env.PORT || 5000))
+});
