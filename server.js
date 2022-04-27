8c5f99c9e371eb98d31cc6b91e6553c2693e5183
vegimselmani <vegim.selmani@drweb.mk>
2022-04-27 14:58:05 +0200

    fixed bugs

--- server.js
+++
@@ -1,9 +0,0 @@
-// server.js  
-const jsonServer = require('json-server')  
-const server = jsonServer.create()
-const middlewares = jsonServer.defaults()  
-   
-server.use(middlewares)  
-server.listen(3001, () => {  
-  console.log('JSON Server is running')  
-})  
