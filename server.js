dfc89022164caa4369eda5b9e3dad65c82d59c1e
vegimselmani <vegim.selmani@drweb.mk>
2022-04-28 00:17:07 +0200

    cors test7

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ var app  = express()
 import cors from 'cors';
 const app = express();
 
-const cors = require('cors');
+app.use(function (req, res, next) {
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-app.get('/', (req, res) => {
-  res.send('CORS solved')
-})
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
