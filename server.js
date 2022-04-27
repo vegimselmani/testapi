40e25e5d25fad349cdb38c34460ce9dff90dcbde
vegimselmani <vegim.selmani@drweb.mk>
2022-04-27 23:43:01 +0200

    cors policy

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ var app  = express()
 var server = app.listen(process.env.PORT || 5000, () => {
     console.log('Server is started on 127.0.0.1:'+ (process.env.PORT || 5000))
 })
+
+let headers = new Headers();
+  headers.append('Access-Control-Allow-Origin');
